73ec0a8a779d9952d34e84c996ecdf76bb087aad
bryevo <ovnairb@gmail.com>
2018-03-24 12:49:27 -0700

    Multi-stream OpenTok Demo

--- src/App.js
+++ src/App.js
@@ -5,13 +5,27 @@ import "./App.css";
 import Test from "./components/Test";
 
 class App extends Component {
+  subscriberProperties = {
+    preferredFrameRate: 15,
+    showControls: false
+  };
+
+  subscriberEventHandlers = {
+    videoDisabled: event => {
+      console.log("Subscriber video disabled!");
+    },
+    videoEnabled: event => {
+      console.log("Subscriber video enabled!");
+    }
+  };
+
   render() {
     return (
       <div className="App">
-        <OTSession apiKey={process.env.REACT_APP_TOK_KEY} sessionId={process.env.REACT_APP_SESSION_ID} token={process.env.REACT_APP_TOK_SECRET}>
+        <OTSession apiKey={process.env.REACT_APP_API_KEY} sessionId={process.env.REACT_APP_SESSION_ID} token={process.env.REACT_APP_TOKEN_ID}>
           <OTPublisher />
           <OTStreams>
-            <OTSubscriber />
+            <OTSubscriber properties={this.subscriberProperties} eventHandlers={this.subscriberEventHandlers} />
           </OTStreams>
         </OTSession>
       </div>
