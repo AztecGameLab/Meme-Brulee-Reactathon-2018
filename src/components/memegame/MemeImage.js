8c4142c8007a3f254795d8af5297fa85aa173954
AC De Leon <andrewchristopherdeleon@gmail.com>
2018-03-25 08:04:53 -0700

    [*] Carousel for meme images

--- src/components/memegame/MemeImage.js
+++ src/components/memegame/MemeImage.js
@@ -6,7 +6,7 @@ import { selectCompletedMemes } from "../../features/meme/memeSelectors";
 
 class MemeImage extends Component {
   render() {
-    return <div>{this.props.completedMemes[0] && <img src={this.props.completedMemes[0].url} />}</div>;
+    return <div>{this.props.completedMemes[this.props.completedMemes.length-1] && <img src={this.props.completedMemes[this.props.completedMemes.length-1].url}/>}</div>;
   }
 }
 
